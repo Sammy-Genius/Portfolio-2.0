3e3af57b3809169e01e34b5f44c121e935433573
sammy-genius <mrsammadeit@gmail.com>
2023-09-03 15:42:48 +0000

    fixed Ui for mobile menu

--- components/MenuForSmallScreens.jsx
+++ components/MenuForSmallScreens.jsx
@@ -4,8 +4,8 @@ const MenuForSmallScreens = () => {
     return ( 
         <>
             <div className="clip-circle-menu flex flex-col justify-center lg:hidden">
-                <span className="text-white absolute top-[5%] right-[10%] text-2xl close-btn">&times;</span>
-                <div className="w-[80%] h-[50%] text-white text-3xl flex flex-col items-center justify-evenly mx-auto my-0">
+                <span className="text-white absolute top-[5%] right-[10%] text-3xl close-btn">&times;</span>
+                <div className="w-[80%] h-[50%] text-white text-4xl flex flex-col items-center justify-evenly mx-auto my-0">
                     <div className="w-auto h-auto overflow-hidden">
                         <li className="translate-y-0 list-none menu-links">
                             <Link href="/">Home</Link>
